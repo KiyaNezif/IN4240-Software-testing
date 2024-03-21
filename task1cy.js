6877794dcba3eebbe80745aeb35d008d3f606897
Yoana Svetoslavova Adriyanova <yoanasa@ulrik.uio.no>
2024-03-21 15:31:49 +0100

    v01

---
+++ task1cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+describe('Testing Automation example',()=>
+{
+    it('Test case for Registration',()=>
+    {
+cy.visit("https://parabank.parasoft.com/parabank/index.htm");
+cy.get('').click();
+    })
+})
